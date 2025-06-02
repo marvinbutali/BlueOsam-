f53c4d948dbc527aeb85a908a754c31d74b4fc1a
bluepen2002 <derrickjunior251@gmail.com>
2025-06-03 00:08:45 +0300

    Add main app file.

--- App.tsx
+++ App.tsx
@@ -1,1 +1,19 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import DerivWS from './utils/DerivWS';
+import { storeAuthTokens } from './utils/derivAuth';
 
+export default function App() {
+  useEffect(() => {
+    const appId = 'YOUR_APP_ID';
+    const ws = new DerivWS(appId);
+    ws.setGlobalHandler((data) => console.log('Global message:', data));
+    storeAuthTokens([{ account: 'demo', token: 'YOUR_TOKEN', currency: 'USD' }]);
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Deriv WS Test App</Text>
+    </View>
+  );
+    }
